511bd1080e5f741c9b74278d524778e6c10fafcb
ani18605 <152686668+ani18605@users.noreply.github.com>
2024-02-08 17:47:12 +0530

    script.js

--- script.js
+++ script.js
@@ -25,3 +25,45 @@
       button.src = flag ? "state_clicked.png" : "heart.png";
       flag = !flag;
     });
+    const postButton = document.querySelector('.post-btn');
+postButton.addEventListener('click', () => {
+    const postContent = document.getElementById('typebox').value;
+    
+    if (postContent.trim() !== '') {
+        const newPost = document.createElement('div');
+        newPost.classList.add('posted');
+        newPost.innerHTML = `
+        <div class="start">
+        <img src="profile_image.png">
+        <h5>Aniruddha Patil @ani18605</h5>
+        <button class="edit"><img src="edit.png"></button>
+        <button class="delete"><img src="delete.png"></button>
+        <button class="reply">Reply</button>
+      </div>
+      <div class="comment">
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Modi, nam.</p>
+      </div>
+      <div class="like">
+        <button class="liked"><img src="heart.png"></button>
+      </div>
+        `;
+        document.querySelector('.container').appendChild(newPost);
+        document.getElementById('typebox').value = '';
+    } else {
+        alert('Please enter some content to post.');
+    }
+});
+const replyButtons = document.querySelectorAll('.reply');
+replyButtons.forEach(replyButton => {
+    replyButton.addEventListener('click', () => {
+        const replyContent = prompt('Etr your reply:');
+        if (replyContent !== null && replyContent.trim() !== '') {
+            const replyElement = document.createElement('div');
+            replyElement.classList.add('reply');
+            replyElement.innerHTML = replyContent;
+            replyButton.parentElement.parentElement.appendChild(replyElement);
+        } else if (replyContent !== null && replyContent.trim() === '') {
+            alert('Please etr some text');
+        }
+    });
+});
